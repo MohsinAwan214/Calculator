4545c1296f2aea4751ca2d6abb08655d31e306ce
MohsinAwan214 <nomanmalik5320@gmail.com>
2024-10-09 02:52:45 -0700

    add Chnages

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const from = document.querySelector("#input_from");
 const boxes1 = document.querySelector("#boxes1");
 const boxes2 = document.querySelector("#boxes2");
-const sum = document.querySelector("#sum");
+const sum1 = document.querySelector("#sum1");
+const sum2 = document.querySelector("#sum2");
+const sum3 = document.querySelector("#sum3");
 const result = document.querySelector(".result");
 
 
@@ -18,8 +20,8 @@ from.addEventListener("submit", (event) => {
   for (let i = 0; i < matrix; i++) {
     for (let j = 0; j < matrix; j++) {
       
-      boxes1.innerHTML += `<input type="number"id="box1-${i}${j}"value="0"style="width: 25px;hight:20px;">`;
-      boxes2.innerHTML += `<input type="number"id="box2-${i}${j}"value="0"style="width: 25px;hight:20px;">`;
+      boxes1.innerHTML += `<input type="number"id="box1-${i}${j}"value="0"style="width: 40px;hight:20px;">`;
+      boxes2.innerHTML += `<input type="number"id="box2-${i}${j}"value="0"style="width: 40px;hight:20px;">`;
     }
     
     boxes1.innerHTML += "<br/>";
@@ -30,15 +32,19 @@ from.addEventListener("submit", (event) => {
 });
 
 
-
-
-sum.addEventListener("click", (app) => {
-
-
-getSum()
+sum1.addEventListener("click", () => {
+getSum1()
+});
+sum2.addEventListener("click", () => {
+getSum2()
+});
+sum3.addEventListener("click", () => {
+getSum3()
 });
 
-let getSum = ()=>{
+
+
+let getSum1 = ()=>{
   result.innerHTML = ""
   for (let i = 0; i < matrix; i++) {
     for (let j = 0; j < matrix; j++) {
@@ -46,7 +52,35 @@ let getSum = ()=>{
        let value2 = document.querySelector(`#box2-${i}${j}`).value;
 
      let a = Number(value1) + Number(value2)  
-     result.innerHTML += `<input style="width: 40px;hight:30px; readonly id="" value="${a}">`
+     result.innerHTML += `<input style="width:40px;hight:30px; color="black" readonly value="${a}">`
+
+}
+result.innerHTML += '<br/>'
+}
+}
+let getSum2 = ()=>{
+  result.innerHTML = ""
+  for (let i = 0; i < matrix; i++) {
+    for (let j = 0; j < matrix; j++) {
+       let value1 = document.querySelector(`#box1-${i}${j}`).value;
+       let value2 = document.querySelector(`#box2-${i}${j}`).value;
+
+     let b = Number(value1) - Number(value2)  
+     result.innerHTML += `<input style="width: 40px;hight:30px; color="black" readonly value="${b}">`
+
+}
+result.innerHTML += '<br/>'
+}
+}
+let getSum3 = ()=>{
+  result.innerHTML = ""
+  for (let i = 0; i < matrix; i++) {
+    for (let j = 0; j < matrix; j++) {
+       let value1 = document.querySelector(`#box1-${i}${j}`).value;
+       let value2 = document.querySelector(`#box2-${i}${j}`).value;
+
+     let c = Number(value1) * Number(value2)  
+     result.innerHTML += `<input style="width: 40px;hight:30px;color="black" readonly value="${c}">`
 
 }
 result.innerHTML += '<br/>'
